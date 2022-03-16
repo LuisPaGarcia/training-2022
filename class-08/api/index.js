39f424044fa940000d31a223c37ef2f5c1a7c3fe
LuisPa <diganluispa@gmail.com>
2022-03-15 19:02:55 -0600

    feat: Adding comments to API

--- class-08/api/index.js
+++ class-08/api/index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 5000;
 /**
  * CORS middleware
+ * https://developer.mozilla.org/es/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
  */
 function cors(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -11,25 +12,36 @@ function cors(req, res, next) {
   next();
 }
 
+// Using the middlewares
 app.use(cors);
 app.use(express.urlencoded({ extended: true })); // To expect JSON as body
 app.use(express.json()); // To parse the incoming requests with JSON payloads
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+// Creating our first GET API
+app.get("/", (request, response) => {
+  response.status(200).send("Hello World!");
 });
 
-app.post("/message/", (req, res) => {
-  if (req.body && req.body.message) {
-    res.status(200).json({
+// Creating our first POST API
+app.post("/message/", (request, response) => {
+  // Using the request
+  if (request.body && request.body.message) {
+    // Invoke que response
+    response.status(200).json({
       error: null,
-      data: { id: crypto.randomUUID(), message: req.body.message },
+      data: {
+        id: crypto.randomUUID(),
+        message: request.body.message,
+      },
     });
     return;
   }
-  res.status(404).json({ error: "Expected to recieve `message`", data: null });
+
+  // If the request doesn't have a body, we return an 404 with the following message
+  response.status(404).json({ error: "Expected to recieve `message`", data: null });
 });
 
+// Starting our API server
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
 });
